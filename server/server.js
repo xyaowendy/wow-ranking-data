96e03bdeda57e957c6c49baf9340ef5a42f20025
Wendy Jiang <wendyjiang0611@gmail.com>
2022-09-22 21:22:14 +1000

    Integrated with mongoose #1

--- server/server.js
+++ server/server.js
@@ -1,9 +1,35 @@
 const path = require('path');
 const express = require("express");
+const mongoose = require('mongoose');
+const {Schema} = require("mongoose");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+// connect to mongo db
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb+srv://admin:oo4KBdt7tSjF2ADr@mycluster.mbzbjc0.mongodb.net/wowdb?retryWrites=true&w=majority');
+}
+
+
+const connection = mongoose.connection;
+
+connection.on('error', console.error.bind(console, 'connection error:'));
+connection.once('open', async function () {
+
+    const collection  = connection.db.collection("user");
+    collection.find({}).toArray(function(err, data){
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(data)
+        }
+    });
+});
+
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
